f51dc88c6a0c6f509de27a8b0b0afa428a820c8f
Bendik Solheim <bendik@solheim.im>
2017-02-20 23:33:18 +0100

    Move backup settings back into config.el

--- config.el
+++ config.el
@@ -2,6 +2,29 @@
       initial-scratch-message nil
       initial-major-mode 'org-mode)
 
+;; Backup files
+;; Make a special "per session" backup at the first save of each
+;; emacs session.
+(defun init/force-backup-of-buffer ()
+  (when (not buffer-backed-up)
+    (let ((backup-directory-alist '(("" . "~/.emacs.d/backups/per-session")))
+        (kept-new-versions 3))
+      (backup-buffer)))
+  
+  (let ((buffer-backed-up nil))
+    (backup-buffer)))
+
+(defun init/setup-backups ()
+  (setq version-control t     ;; Use version numbers for backups
+    kept-new-versions 10  ;; Number of newest versions to keep
+    kept-old-versions 0   ;; Number of oldest versions to keep
+    delete-old-versions t ;; Don’t ask to delete excess backup versions
+    backup-by-copying t   ;; Copy all files, don’t rename them
+    backup-directory-alist '(("" . "~/.emacs.d/backups/per-save"))
+    vc-make-backup-files t))
+
+(add-hook 'before-save-hook 'force-backup-of-buffer)
+
 ;; Package Management
 (require 'package)
 (setq package-enable-at-startup nil)
--- init.d/init-settings.el
+++ init.d/init-settings.el
@@ -1,25 +1,3 @@
-(defun init/force-backup-of-buffer ()
-  ;; Make a special "per session" backup at the first save of each
-  ;; emacs session.
-  (when (not buffer-backed-up)
-    (let ((backup-directory-alist '(("" . "~/.emacs.d/backups/per-session")))
-        (kept-new-versions 3))
-      (backup-buffer)))
-  
-  (let ((buffer-backed-up nil))
-    (backup-buffer)))
-
-(defun init/setup-backups ()
-  (setq version-control t     ;; Use version numbers for backups
-    kept-new-versions 10  ;; Number of newest versions to keep
-    kept-old-versions 0   ;; Number of oldest versions to keep
-    delete-old-versions t ;; Don’t ask to delete excess backup versions
-    backup-by-copying t   ;; Copy all files, don’t rename them
-    backup-directory-alist '(("" . "~/.emacs.d/backups/per-save"))
-    vc-make-backup-files t))
-
-(add-hook 'before-save-hook 'force-backup-of-buffer)
-
 ;; Yes or no prompts
 (fset 'yes-or-no-p 'y-or-n-p)
 
