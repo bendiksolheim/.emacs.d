bfca090371c91eefb8369704eedcb2637477fe6f
Bendik Solheim <bendik@solheim.im>
2017-03-08 21:02:45 +0100

    Elm mode changes

--- init.d/init-elm.el
+++ init.d/init-elm.el
@@ -1,9 +1,21 @@
 (require 'req-package)
 
 (req-package elm-mode
-  :require (f s let-alist)
+  :require (f s let-alist company)
   :mode ("\\.elm\\'" . elm-mode)
   :config
-  (setq elm-format-on-save t))
+  (setq elm-format-on-save t)
+  (add-hook 'elm-mode-hook
+            (lambda ()
+              (setq-local company-backends
+                          (append
+                            '(company-elm)
+                            company-backends))))
+  (add-hook 'elm-mode-hook 'elm-oracle-setup-completion))
+
+(req-package flycheck-elm
+  :require (flycheck elm-mode)
+  :init
+  (add-hook 'flycheck-mode-hook 'flycheck-elm-setup t))
 
 (provide 'init-elm)
