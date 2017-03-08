f28c521dd21e5db5df2f9458cb3d130b4eba2b60
Bendik Solheim <bendik@solheim.im>
2017-03-08 21:03:06 +0100

    Flycheck

--- init.d/init-flycheck.el
+++ init.d/init-flycheck.el
@@ -2,13 +2,11 @@
 
 (req-package flycheck
   :diminish flycheck-mode
-  :init
-  (global-flycheck-mode t)
   :config
+  (global-flycheck-mode t)
   (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc
                                              javascript-jshint
-                                             json-jsonlint)
-		flycheck-c/c++-gcc-executable "gcc"))
+                                             json-jsonlint)))
 
 (req-package helm-flycheck
  :require flycheck)
