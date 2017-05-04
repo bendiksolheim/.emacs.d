a745e57cf89547e82138cb9d41859277bcd59914
Bendik Solheim <bendik@solheim.im>
2017-05-04 10:29:11 +0200

    Some path loading stuff

--- init.d/init-general.el
+++ init.d/init-general.el
@@ -3,5 +3,10 @@
 (req-package s)
 (req-package f)
 (req-package let-alist)
+(req-package exec-path-from-shell
+  :force
+  :config
+  (when (memq window-system '(mac ns))
+    (exec-path-from-shell-initialize)))
 
 (provide 'init-general)
