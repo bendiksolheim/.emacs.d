431bd36328218a2d3afead4f44b36677564fb7d8
Bendik Solheim <bendik@solheim.im>
2017-07-31 15:25:49 +0200

    Make helm behave even better..

--- init.d/init-shackle.el
+++ init.d/init-shackle.el
@@ -1,8 +1,10 @@
 (require 'req-package)
 
 (req-package shackle
+  :require helm
   :config
   (shackle-mode 1)
+  (setq helm-display-function 'pop-to-buffer)
   (setq shackle-rules
         '(("\\`\\*helm.*?\\*\\'" :regexp t :align t :ratio 0.4))))
 
