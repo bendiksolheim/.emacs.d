0f0c02f32199b83da71db445317f956cc34496fa
Bendik Solheim <bendik@solheim.im>
2017-08-22 12:51:04 +0200

    Mark doom-one theme as safe

--- custom.el
+++ custom.el
@@ -3,6 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("12e2aee98f651031d10fd58af76250fa8cab6f28b3e88f03b88b7524c9278549" default)))
  '(package-selected-packages
    (quote
     (spacemacs-theme powerline markdown-mode evil-leader ace-window relative-line-numbers which-key evil-god-state projectile evil helm))))
