4f578bb9bec2a9aae20218b16915828b0dd01993
Bendik Solheim <bendik@solheim.im>
2017-10-25 11:14:37 +0200

    Start on `helm-projectile-ag` substitute

---
+++ init.d/init-helm-project-ag.el
@@ -0,0 +1,60 @@
+(require 'helm)
+(require 'helm-grep)
+(require 'cl-lib)
+(require 'projectile)
+
+(defvar helm-project-ag-base-command
+  `("ag" "--nogroup" "--nocolor")
+  "Base `ag` command")
+
+(defvar helm-project-ag-input-min 3
+  "Minimum amount of characters to input before search starts")
+
+(defvar helm-project-ag-ignore-list
+  `(".git" ".svn" ".hg" "*.class")
+  "List of ignored files and folders")
+
+(defun helm-project-ag--ignore (pattern)
+  "Construct ignore pattern"
+  (concat "--ignore " pattern))
+
+(defun helm-project-ag--process ()
+  "Run ag process based on search input"
+  (let* ((base-command helm-project-ag-base-command)
+         (ignores (split-string (mapconcat 'helm-project-ag--ignore helm-project-ag-ignore-list " ")))
+         (query `(,helm-pattern))
+         (root `(,(init/project-root))) ; TODO: init/project-root exists globally, should probably make possible to import..
+         (cmd (append base-command ignores query root)))
+    (apply #'start-process "ag" nil cmd)))
+
+(defvar helm-project-ag--action
+  (helm-make-actions
+   "Open file" (lambda (candidate) (message (concat "Run action: " candidate))))
+  "Run helm action on item")
+
+;; More or less copied from helm-ag.el
+(defun helm-project-ag--display-filter (item)
+  "Filter ech item"
+  (helm-aif (helm-grep-split-line item)
+      (format "%s:%s:%s"
+              (propertize (cl-first it) 'face 'helm-moccur-buffer)
+              (propertize (cl-second it) 'face 'helm-grep-lineno)
+              (cl-third it))))
+
+(defvar helm-project-ag--source
+  (helm-build-async-source "Search"
+    :candidates-process 'helm-project-ag--process
+    :action 'helm-project-ag--action
+    :candidate-number-limit 999
+    :filter-one-by-one 'helm-project-ag--display-filter
+    :requires-pattern helm-project-ag-input-min)
+  "Helm source to search projectile folder with ag")
+
+(defun helm-project-ag ()
+  "Search project with `ag` and display results in helm"
+  (interactive)
+  (helm :sources '(helm-project-ag--source)
+        :buffer "*helm projectile search*"))
+
+(provide 'init-helm-project-ag)
+
