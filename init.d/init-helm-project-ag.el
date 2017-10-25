2efe772da8aeba9de45e9ddc45c166d4f55d1b1c
Bendik Solheim <bendik@solheim.im>
2017-10-25 21:27:04 +0200

    Actually open file when pressing enter

--- init.d/init-helm-project-ag.el
+++ init.d/init-helm-project-ag.el
@@ -7,7 +7,8 @@
   `("ag" "--nogroup" "--nocolor")
   "Base `ag` command")
 
-(defvar helm-project-ag-input-min 3
+(defvar helm-project-ag-input-min
+  3
   "Minimum amount of characters to input before search starts")
 
 (defvar helm-project-ag-ignore-list
@@ -17,7 +18,7 @@
 (defun helm-project-ag--ignore (pattern)
   "Construct ignore pattern"
   (concat "--ignore " pattern))
-
+ 
 (defun helm-project-ag--process ()
   "Run ag process based on search input"
   (let* ((base-command helm-project-ag-base-command)
@@ -27,10 +28,19 @@
          (cmd (append base-command ignores query root)))
     (apply #'start-process "ag" nil cmd)))
 
+(defun helm-project-ag--open-file (candidate)
+  "Open file in search candidate and go to correct line"
+  (let* ((split-candidate (helm-grep-split-line candidate))
+         (filename (cl-first split-candidate))
+         (line (cl-second split-candidate)))
+    (find-file filename)
+    (goto-char (point-min))
+    (forward-line (1- (string-to-number line)))))
+
 (defvar helm-project-ag--action
   (helm-make-actions
-   "Open file" (lambda (candidate) (message (concat "Run action: " candidate))))
-  "Run helm action on item")
+   "Open file" 'helm-project-ag--open-file)
+  "Helm actions")
 
 ;; More or less copied from helm-ag.el
 (defun helm-project-ag--display-filter (item)
