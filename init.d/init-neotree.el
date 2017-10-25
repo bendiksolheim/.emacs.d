14e5d5205bc94fa2b67e558512bc94366da51726
Bendik Solheim <bendik@solheim.im>
2017-10-25 11:13:52 +0200

    Sensible neotree hotkeys

--- init.d/init-neotree.el
+++ init.d/init-neotree.el
@@ -1,8 +1,21 @@
 (require 'req-package)
 
 (req-package neotree
-  :defer t
+  :require evil-leader
   :init
-  (setq neo-theme 'nerd));
+  (setq neo-theme 'nerd)
+  :config
+  (add-hook 'neotree-mode-hook (lambda ())
+    (dolist (key '(("TAB" . 'neotree-stretch-toggle)
+                   ("RET" . 'neotree-enter)
+                   ("|" . 'neotree-enter-vertical-split)
+                   ("-" . 'neotree-enter-horizontal-split)
+                   ("c" . 'neotree-create-node)
+                   ("d" . 'neotree-delete-node)
+                   ("g" . 'neotree-refresh)
+                   ("j" . 'neotree-next-line)
+                   ("k" . 'neotree-previous-line)
+                   ("r" . 'neotree-rename-node)))
+      (eval `(evil-define-key 'normal neotree-mode-map (kbd ,(car key)) ,(cdr key))))))
 
 (provide 'init-neotree)
