c2554537121d9542f5bbb53cd3812ebd90b4d118
Bendik Solheim <bendik@solheim.im>
2017-10-31 23:24:44 +0100

    Use ace-window instead of window-numbering

--- init.d/init-window.el
+++ init.d/init-window.el
@@ -1,8 +1,16 @@
 (require 'req-package)
 
-(req-package window-numbering
+(req-package ace-window
   :config
-  (progn
-    (window-numbering-mode)))
+  (setq aw-dispatch-always t)
+  (defvar aw-dispatch-alist
+    '((?x aw-delete-window "Ace - Delete Window")
+      (?m aw-swap-window "Ace - Swap Window")
+      (?n aw-flip-window "Ace - Previous Window")
+      (?c aw-split-window-fair "Ace - Split Fair Window")
+      (?- aw-split-window-horz "Ace - Split Horizontal Window")
+      (?k aw-split-window-vert "Ace - Split Vertical Window")
+      (?o delete-other-windows "Ace - Delete Other Windows")))
+  (evil-global-set-key 'normal "'" 'ace-window))
 
 (provide 'init-window)
