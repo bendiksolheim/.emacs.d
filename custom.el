4bcd92302c9b5b03772cd079a8bc7e019954f1b9
Bendik Solheim <bendik@solheim.im>
2017-11-05 22:28:46 +0100

    Custom stuff

--- custom.el
+++ custom.el
@@ -5,10 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("4182c491b5cc235ba5f27d3c1804fc9f11f51bf56fb6d961f94788be034179ad" "12e2aee98f651031d10fd58af76250fa8cab6f28b3e88f03b88b7524c9278549" default)))
+    ("9f569b5e066dd6ca90b3578ff46659bc09a8764e81adf6265626d7dc0fac2a64" "4182c491b5cc235ba5f27d3c1804fc9f11f51bf56fb6d961f94788be034179ad" "12e2aee98f651031d10fd58af76250fa8cab6f28b3e88f03b88b7524c9278549" default)))
  '(package-selected-packages
    (quote
-    (spacemacs-theme powerline markdown-mode evil-leader ace-window relative-line-numbers which-key evil-god-state projectile evil helm))))
+    (helm-ag-r spacemacs-theme powerline markdown-mode evil-leader ace-window relative-line-numbers which-key evil-god-state projectile evil helm)))
+ '(tramp-syntax (quote default) nil (tramp)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
