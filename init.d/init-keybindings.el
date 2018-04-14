2f9740bba0ce80f4e07b29dc7135dfa72fcc4f96
Bendik Solheim <bendik@solheim.im>
2018-04-14 08:17:43 +0200

    Shortcuts for cycling between buffers

--- init.d/init-keybindings.el
+++ init.d/init-keybindings.el
@@ -10,6 +10,8 @@
   (setq text-scale-mode-step 1.1)
   (global-set-key (kbd "s-+") (lambda () (interactive) (text-scale-increase 1)))
   (global-set-key (kbd "s--") (lambda () (interactive) (text-scale-decrease 1)))
-  (global-set-key (kbd "s-0") (lambda () (interactive) (text-scale-set 0))))
+  (global-set-key (kbd "s-0") (lambda () (interactive) (text-scale-set 0)))
+  (global-set-key (kbd "s-j") 'next-buffer)
+  (global-set-key (kbd "s-k") 'previous-buffer))
 
 (provide 'init-keybindings)
