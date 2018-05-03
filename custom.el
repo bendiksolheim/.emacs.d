10e8306fb1400d8b28fd100a0f25974346c665e7
Bendik Solheim <bendik@solheim.im>
2018-05-03 22:02:13 +0200

    Install restclient

--- custom.el
+++ custom.el
@@ -8,7 +8,7 @@
     ("10e3d04d524c42b71496e6c2e770c8e18b153fcfcc838947094dad8e5aa02cef" "9f569b5e066dd6ca90b3578ff46659bc09a8764e81adf6265626d7dc0fac2a64" "4182c491b5cc235ba5f27d3c1804fc9f11f51bf56fb6d961f94788be034179ad" "12e2aee98f651031d10fd58af76250fa8cab6f28b3e88f03b88b7524c9278549" default)))
  '(package-selected-packages
    (quote
-    (remark-mode all-the-icons helm-ag-r spacemacs-theme powerline markdown-mode evil-leader ace-window relative-line-numbers which-key evil-god-state projectile evil helm)))
+    (restclient remark-mode all-the-icons helm-ag-r spacemacs-theme powerline markdown-mode evil-leader ace-window relative-line-numbers which-key evil-god-state projectile evil helm)))
  '(tramp-syntax (quote default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
---
+++ init.d/init-restclient.el
@@ -0,0 +1,5 @@
+(require 'req-package)
+
+(req-package restclient)
+
+(provide 'rest-client)
