887e80a31d84da4a6b4fe16418aca301ef074a9f
Bendik Solheim <bendik@solheim.im>
2018-10-26 22:32:36 +0200

    dunno

--- custom.el
+++ custom.el
@@ -5,10 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("10e3d04d524c42b71496e6c2e770c8e18b153fcfcc838947094dad8e5aa02cef" "9f569b5e066dd6ca90b3578ff46659bc09a8764e81adf6265626d7dc0fac2a64" "4182c491b5cc235ba5f27d3c1804fc9f11f51bf56fb6d961f94788be034179ad" "12e2aee98f651031d10fd58af76250fa8cab6f28b3e88f03b88b7524c9278549" default)))
+    ("6b2636879127bf6124ce541b1b2824800afc49c6ccd65439d6eb987dbf200c36" "10e3d04d524c42b71496e6c2e770c8e18b153fcfcc838947094dad8e5aa02cef" "9f569b5e066dd6ca90b3578ff46659bc09a8764e81adf6265626d7dc0fac2a64" "4182c491b5cc235ba5f27d3c1804fc9f11f51bf56fb6d961f94788be034179ad" "12e2aee98f651031d10fd58af76250fa8cab6f28b3e88f03b88b7524c9278549" default)))
  '(package-selected-packages
    (quote
-    (flycheck-kotlin kotlin-mode restclient remark-mode all-the-icons helm-ag-r spacemacs-theme powerline markdown-mode evil-leader ace-window relative-line-numbers which-key evil-god-state projectile evil helm)))
+    (web-mode rjsx-mode neotree dumb-jump doom-themes evil-magit evil-surround evil-anzu telephone-line helm-projectile magit prettier-js shackle helm-swoop exec-path-from-shell f helm-flycheck intero linum-relative company avy ag flycheck helm-grepint flycheck-kotlin kotlin-mode restclient remark-mode all-the-icons helm-ag-r spacemacs-theme powerline markdown-mode evil-leader ace-window relative-line-numbers which-key evil-god-state projectile evil helm)))
  '(tramp-syntax (quote default) nil (tramp)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
