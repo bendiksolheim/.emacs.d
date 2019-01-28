c4e2b147da2c2311eca691fdf943aa976db6acf5
Bendik Solheim <bendik@solheim.im>
2019-01-28 20:57:30 +0100

    Disable linum mode in magit. Actually working this time

--- init.d/init-general.el
+++ init.d/init-general.el
@@ -6,13 +6,18 @@
 ;;   (when (memq window-system '(mac ns))
 ;;     (exec-path-from-shell-initialize)))
 
-(define-global-minor-mode custom-linum-relative-global-mode linum-mode
-  (lambda ()
-    (when (not (memq major-mode
-                     (list 'magit-status-mode))))))
+(defun custom-linum-relative-global-mode ()
+  "Custom linum relative global mode which won’t load in magit"
+  (add-hook 'after-change-major-mode-hook
+            (lambda () (linum-relative-mode 0))
+            :append :local))
 
 (use-package linum-relative
+  :ensure t
+  :init
+  (setq linum-relative-backend 'display-line-numbers-mode)
   :config
-  (custom-linum-relative-global-mode))
+  (linum-relative-global-mode)
+  (add-hook 'magit-mode-hook  'custom-linum-relative-global-mode))
 
 (provide 'init-general)
