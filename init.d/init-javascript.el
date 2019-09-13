f25d7e50628902bd925232bd6e1c833621c63a19
Bendik Solheim <hello@bendik.dev>
2019-09-13 10:02:17 +0200

    Try web-mode for js and jsx

--- init.d/init-javascript.el
+++ init.d/init-javascript.el
@@ -1,18 +1,24 @@
-(use-package js2-mode
-  :mode "\\.jsx?\\'"
+(use-package web-mode
+  :mode "\\.jsx?$"
   :init
-  (setq js2-mode-show-strict-warnings nil
-        js2-mode-show-parse-errors nil
-        js2-basic-offset 2
-        sgml-basic-offset 2
-        js-indent-level 2))
+  (setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'"))
+        web-mode-markup-indent-offset 4))
 
-(use-package rjsx-mode
-  :mode "\\.jsx?\\'"
-  :commands (rjsx-mode))
+;; (use-package js2-mode
+;;   :mode "\\.jsx?\\'"
+;;   :init
+;;   (setq js2-mode-show-strict-warnings nil
+;;         js2-mode-show-parse-errors nil
+;;         js2-basic-offset 2
+;;         sgml-basic-offset 2
+;;         js-indent-level 2))
 
-(use-package json-mode
-  :mode "\\.json\\'")
+;; (use-package rjsx-mode
+;;   :after js2-mode
+;;   :hook (js2-mode . rjsx-minor-mode))
+
+;; (use-package json-mode
+;;   :mode "\\.json\\'")
 
 (use-package prettier-js
   :init
@@ -20,9 +26,8 @@
                            "--single-quote"
                            "--tab-width" "2"
                            ))
-  :config
-  (add-hook 'js2-mode-hook 'prettier-js-mode)
-  (add-hook 'css-mode-hook 'prettier-js-mode)
-  (add-hook 'typescript-mode 'prettier-js-mode))
+  :hook ((web-mode . prettier-js-mode)
+         (typescript-mode . prettier-js-mode)
+         (css-mode . prettier-js-mode)))
 
 (provide 'init-javascript)
