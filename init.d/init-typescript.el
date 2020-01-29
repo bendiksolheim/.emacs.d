0c6aa552ef70b948b8e78bb18153aedb43a5e6f8
Bendik Solheim <hello@bendik.dev>
2020-01-29 20:00:18 +0100

    Remove tide package

--- init.d/init-typescript.el
+++ init.d/init-typescript.el
@@ -6,12 +6,4 @@
   :init
   (setq typescript-indent-level 2))
 
-(defun setup-tide-mode ()
-  (interactive)
-  (tide-setup)
-  (tide-hl-identifier-mode +1))
-
-(use-package tide
-  :hook (typescript-mode . setup-tide-mode))
-
 (provide 'init-typescript)
