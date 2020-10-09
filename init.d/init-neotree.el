1c4f90449e931a1a04a2fcf986b7b33d114930e0
Bendik Solheim <hello@bendik.dev>
2020-10-09 21:56:43 +0200

    Support neotree resizing. Fix some neotree hotkeys

--- init.d/init-neotree.el
+++ init.d/init-neotree.el
@@ -2,14 +2,17 @@
 
 (use-package neotree
   :init
-  (setq neo-theme 'nerd)
+  (setq neo-theme 'nerd
+        neo-window-fixed-size nil)
   :config
   (add-hook 'neotree-mode-hook (lambda ())
     (dolist (key '(("TAB" . 'neotree-stretch-toggle)
                    ("RET" . 'neotree-enter)
+                   ("l" . 'neotree-quick-look)
                    ("|" . 'neotree-enter-vertical-split)
                    ("-" . 'neotree-enter-horizontal-split)
-                   ("c" . 'neotree-create-node)
+                   ("c" . 'neotree-copy-node)
+                   ("n" . 'neotree-create-node)
                    ("d" . 'neotree-delete-node)
                    ("g" . 'neotree-refresh)
                    ("j" . 'neotree-next-line)
